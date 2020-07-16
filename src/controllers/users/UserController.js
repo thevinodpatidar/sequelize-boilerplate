f5b1d25af6a43d34ab189d0e81cf476d8a57beae
vinodpatidar123 <pvinod813@gmail.com>
2020-07-16 22:52:54 +0530

    added user

--- src/controllers/users/UserController.js
+++ src/controllers/users/UserController.js
@@ -1,19 +1,32 @@
 const UserModel = require("../../models/userModel");
 
+
 const CreateUser = async(req,res)=>{
     const jane = await UserModel.create({ firstName: "Jane", lastName: "Doe" });
     res.status(200).json({"Jane's auto-generated ID:": jane.id});
 }
 
 const GetUsers = async(req,res)=>{
-    const users = await UserModel.findAll();
+    const users = await UserModel.findAll({});
 
     res.status(200).json({
         users
     })
 }
 
+const RemoveUser = async(req,res)=>{
+    const removedUser = await UserModel.destroy({
+        where:{
+            firstName : "Jane"
+        }
+    })
+
+    res.status(200).send(
+        "User Removed");
+}
+
 module.exports = {
     CreateUser,
-    GetUsers
+    GetUsers,
+    RemoveUser
 }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,5 +5,6 @@ const UserController = require("../controllers/index").UserController;
 
 router.post("/user",UserController.CreateUser);
 router.get("/user",UserController.GetUsers);
+router.delete("/user",UserController.RemoveUser);
 
 module.exports = router;
